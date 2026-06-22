b32a60c377a9dda79030d2b24885e02f1ce2f1f2
Vedant_Karokar <vedantworks2@gmail.com>
2026-06-22 17:11:03 +0530

    added the code and docker file

---
+++ bronze.py
@@ -0,0 +1,94 @@
+import os
+import pandas as pd
+import boto3
+from dotenv import load_dotenv
+from io import StringIO
+from sqlalchemy import create_engine
+from datetime import datetime, timedelta
+from pydantic import BaseModel
+
+load_dotenv()
+
+# -------------CONFIGURATION-------------
+
+db_config={
+    "user": os.getenv("MYSQL_USER"),
+    "host": os.getenv("MYSQL_HOST"),
+    "port": os.getenv("MYSQL_PORT"),
+    "database": os.getenv("MYSQL_DATABASE"),
+    "password": os.getenv("MYSQL_ROOT_PASSWORD")
+}
+
+aws_bucket="careplusstorage"
+aws_prefix="support-tickets/raw/"
+date_file="date_tracker.txt"
+
+aws_config={
+    "secret_key" : os.getenv("AWS_secret_key"),
+    "access_key" : os.getenv("AWS_access_key"),
+    "region" : os.getenv("AWS_region")
+}
+
+# -------------UTILITY FUNCTIONS-------------
+
+def get_engine(config):
+    return create_engine(f"mysql+pymysql://{config['user']}:{config['password']}@{config['host']}:{config['port']}/{config['database']}")
+
+def upload_to_s3(df, bucket, key):
+    # Buffer for AWS
+    csv_buffer=StringIO()
+    df.to_csv(csv_buffer, index=False)
+    s3=boto3.client('s3', config=aws_config)
+    s3.put_object(
+        bucket=bucket,
+        key=key,
+        body=csv_buffer.getvalue()
+    )
+
+def read_last_date(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            return f.read().strip()
+    #return "2025-06-30"  Starting point before 1st July
+
+def update_last_date(file_path, new_date):
+    with open(file_path, 'w') as f:
+        f.write(new_date)
+
+def get_next_date(last_date_str):
+    last_date=datetime.strptime(last_date_str, "%Y-%m-%d")
+    next_date=last_date + timedelta(days=1)
+    return next_date.strftime("%Y-%m-%d")
+
+# -------------MAIN INGESTION LOGIC-------------
+
+def began_ingestion():
+    engine=get_engine(config=db_config)
+    last_date=read_last_date(file_path=date_file)
+    next_date=get_next_date(last_date_str=last_date)
+
+    # Query only specific day’s data
+    query = f"""
+        SELECT * FROM support_tickets:
+        WHERE DATE(created_at) = '{next_date}'
+    """
+    df = pd.read_sql(sql=query, con=engine)
+    if df.empty:
+        print(f"No data found for {next_date}. Skipping upload.")
+        return
+    else:
+        print(df.shape)
+        print(df.head())
+
+    # upload to s3
+    s3_key = f"{aws_prefix}support_tickets_{next_date}.csv"
+    upload_to_s3(df, aws_bucket, s3_key)
+
+    # Update date tracker
+    update_last_date(date_file, next_date)
+    print(f"Updated tracker to {next_date}")
+
+# Run
+# This block ONLY runs when you do: python bronze.py
+if __name__ == "__main__":
+    began_ingestion()
---
+++ support.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE  IF NOT EXISTS `careplus_support_db`;
+USE `careplus_support_db`;
