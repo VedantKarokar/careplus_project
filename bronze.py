291f51a38acd9ac97e2059a62d5a79c030f2b410
Vedant_Karokar <vedantworks2@gmail.com>
2026-07-15 13:42:18 +0530

    added db_ check code into the db_config

--- bronze.py
+++ bronze.py
@@ -4,86 +4,53 @@ import boto3
 from dotenv import load_dotenv
 from io import StringIO
 from sqlalchemy import create_engine
-from datetime import datetime, timedelta
+from boto3.s3.transfer import TransferConfig
+import logging
 
+logging.basicConfig(
+        filename="ingestion.log",
+        level=logging.ERROR,
+        format = "%(asctime)s - %(levelname)s - %(message)s",
+)
 load_dotenv()
 
-# -------------CONFIGURATION-------------
-
 aws_bucket="careplusstorage"
-aws_prefix="support-tickets/raw/"
-date_file="date_tracker.txt"
+aws_prefix="support_tickets/raw/"
 
-aws_config={
-    "secret_key" : 
-    "access_key" : 
-    "region" : 
-}
+engine = create_engine(f"mysql+pymysql://{os.getenv("MYSQL_ROOT_USER")}:{os.getenv("MYSQL_ROOT_PASSWORD")}@{os.getenv("MYSQL_HOST")}:{os.getenv("MYSQL_PORT")}/{os.getenv("MYSQL_DATABASE")}",echo = True)
 
-# -------------UTILITY FUNCTIONS-------------
-
-def get_engine():
-    return create_engine(f"mysql+pymysql://{os.getenv("MYSQL_ROOT_USER")}:{os.getenv("MYSQL_ROOT_PASSWORD")}@{os.getenv("MYSQL_HOST")}:{os.getenv("MYSQL_PORT")}/{os.getenv("MYSQL_DATABASE")}")
-
-def upload_to_s3(df, bucket, key):
+def s3_upload(df, bucket, key):
     # Buffer for AWS
     csv_buffer=StringIO()
-    df.to_csv(csv_buffer, index=False)
+    df.to_csv(
+        csv_buffer,
+        index=False
+        )
     s3=boto3.client(
-        's3', os.getenv("AWS_secret_key"),
-        os.getenv("AWS_access_key"),
-        os.getenv("AWS_region")
+        service_name='s3'
+        )
+    boto3.s3.transfer.TransferConfig(
+        multipart_chunksize=2
         )
     s3.put_object(
-        bucket=bucket,
-        key=key,
-        body=csv_buffer.getvalue()
-    )
-
-def read_last_date(file_path):
-    if os.path.exists(file_path):
-        with open(file_path, 'r') as f:
-            return f.read().strip()
-    #return "2025-06-30"  Starting point before 1st July
-
-def update_last_date(file_path, new_date):
-    with open(file_path, 'w') as f:
-        f.write(new_date)
-
-def get_next_date(last_date_str):
-    last_date=datetime.strptime(last_date_str, "%Y-%m-%d")
-    next_date=last_date + timedelta(days=1)
-    return next_date.strftime("%Y-%m-%d")
-
-# -------------MAIN INGESTION LOGIC-------------
+        Bucket=bucket,
+        Key=key,
+        Body=csv_buffer.getvalue()
+        )
 
 def began_ingestion():
-    engine=get_engine()
-    last_date=read_last_date(file_path=date_file)
-    next_date=get_next_date(last_date_str=last_date)
-
-    # Query only specific day’s data
+    # Query data
     query = f"""
-        SELECT * FROM support_tickets:
-        WHERE DATE(created_at) = '{next_date}'
-    """
+        SELECT * FROM Tickets;
+        """
     df = pd.read_sql(sql=query, con=engine)
     if df.empty:
-        print(f"No data found for {next_date}. Skipping upload.")
-        return
-    else:
-        print(df.shape)
-        print(df.head())
-
+        print(f"No data found, skipping upload.")
     # upload to s3
-    s3_key = f"{aws_prefix}support_tickets_{next_date}.csv"
-    upload_to_s3(df, aws_bucket, s3_key)
-
-    # Update date tracker
-    update_last_date(date_file, next_date)
-    print(f"Updated tracker to {next_date}")
+    s3_key = f"{aws_bucket}/{aws_prefix}"
+    return s3_upload(df, aws_bucket, s3_key)
 
 # Run
 # This block ONLY runs when you do: python bronze.py
-if __name__ == "__main__":
-    began_ingestion()
+# if __name__ == "__main__":
+began_ingestion()
--- db/db_config.py
+++ db/db_config.py
@@ -1,5 +1,9 @@
 import os
-import csv
+import pandas as pd
+import numpy as np
+import logging
+from time import sleep
+from io import StringIO
 from typing import Optional
 from dotenv import load_dotenv
 from sqlalchemy import String
@@ -10,8 +14,13 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
+logging.basicConfig(
+        filename="db.log",
+        level=logging.DEBUG,
+        format = "%(asctime)s - %(levelname)s - %(message)s",
+)
 #Load env variables
-load_dotenv() 
+load_dotenv()
 
 #Define a Model
 class Base(DeclarativeBase):
@@ -42,6 +51,13 @@ class Tickets(Base):
 
         def __repr__(self):
                 return f"Tickets(ticket_id={self.ticket_id!r}, created_at={self.created_at!r}, resolved_at={self.resolved_at!r}), agent={self.agent!r}, priority={self.priority!r}, num_interactions={self.num_interactions!r}, IssueCat={self.IssueCat!r}, channel={self.channel!r}, status={self.status!r}, agent_feedback={self.agent_feedback}"
+print("-------------------------------------------------------")
+print("*")
+sleep(1)
+print("*")
+sleep(1)
+print("*")
+sleep(2)
 
 #Create an engine        
 engine=create_engine(f"mysql+pymysql://{os.getenv("MYSQL_ROOT_USER")}:{os.getenv("MYSQL_ROOT_PASSWORD")}@{os.getenv("MYSQL_HOST")}/{os.getenv("MYSQL_DATABASE")}", echo=True)
@@ -49,13 +65,42 @@ engine=create_engine(f"mysql+pymysql://{os.getenv("MYSQL_ROOT_USER")}:{os.getenv
 #Create the table using defined data model and engine
 Base.metadata.create_all(engine)
 
+print("*")
+sleep(1)
+print("*")
+sleep(1)
+print("*")
+sleep(2)
+
+#Remove duplicates
+with open("data.csv", "r") as f:
+    data = f.read()
+data = StringIO(data)
+df = pd.read_csv(data)
+df = df.drop_duplicates()
+df = df.replace({np.nan: None})
+print("-------------------------------------------------------")
+
+print("*")
+sleep(1)
+print("*")
+sleep(1)
+print("*")
+sleep(2)
+
 #Add data
 BATCH_SIZE = 200
 with Session(engine, autoflush=False) as session:
-        with open("data.csv", "r") as f:
-                reader=csv.DictReader(f)
-                data=[row for row in reader]
+                data = df.to_dict(orient="records")
                 for i in range(0, len(data), BATCH_SIZE):
                         batch = data[i : i + BATCH_SIZE]
                         session.execute(insert(Tickets), batch)
+                        print("*")
+                        sleep(1)
+                        print("*")
+                        sleep(1)
+                        print("*")
+                        sleep(2)
                         session.commit()
+print("-------------------------------------------------------")
+# sqlalchemy.exc.IntegrityError:
